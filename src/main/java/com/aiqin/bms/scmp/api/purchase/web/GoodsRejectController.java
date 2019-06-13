a985adeb78fad76d909efc97d4dff428c1462780
wangximin@aiqin.com <wangximin@aiqin.com>
2019-06-13 14:51:57 +0800

    提交

--- src/main/java/com/aiqin/bms/scmp/api/purchase/web/GoodsRejectController.java
+++ src/main/java/com/aiqin/bms/scmp/api/purchase/web/GoodsRejectController.java
@@ -1,3 +1,9 @@
+package com.aiqin.bms.scmp.api.purchase.web;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * <p>
  * ━━━━━━神兽出没━━━━━━
@@ -24,4 +30,11 @@
  * <p>
  * 思维方式*热情*能力
  */
-package com.aiqin.bms.scmp.api.purchase.service;
+@Api("退供controller")
+@RequestMapping("/reject")
+@RestController
+public class GoodsRejectController {
+
+
+
+}
