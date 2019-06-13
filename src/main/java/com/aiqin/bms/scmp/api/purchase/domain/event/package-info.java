15e3889fd07bf57d5bef251646c62ac670e18f77
wangximin@aiqin.com <wangximin@aiqin.com>
2019-06-13 14:40:31 +0800

    remove

--- src/main/java/com/aiqin/bms/scmp/api/purchase/domain/event/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * @author knight.xie
- * @className package-info
- * @date 2019/6/13 14:03
- * @description 事件
- * @version 1.0
- */
-package com.aiqin.bms.scmp.api.purchase.domain.event;
--- src/main/java/com/aiqin/bms/scmp/api/purchase/mapper/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * @author knight.xie
- * @className package-info
- * @date 2019/6/13 14:05
- * @description mapper
- * @version 1.0
- */
-package com.aiqin.bms.scmp.api.purchase.mapper;
