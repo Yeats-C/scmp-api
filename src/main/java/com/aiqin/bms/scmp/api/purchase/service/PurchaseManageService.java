a5ad206f1cc1c651b5e07369c3ffc9a82b4ecad2
zhaoshuai <zhaoshuai@aiqin.com>
2019-06-25 17:18:26 +0800

    采购文件

--- src/main/java/com/aiqin/bms/scmp/api/purchase/service/PurchaseManageService.java
+++ src/main/java/com/aiqin/bms/scmp/api/purchase/service/PurchaseManageService.java
@@ -1,5 +1,6 @@
 package com.aiqin.bms.scmp.api.purchase.service;
 
+import com.aiqin.bms.scmp.api.purchase.domain.PurchaseFile;
 import com.aiqin.bms.scmp.api.purchase.domain.request.PurchaseFormRequest;
 import com.aiqin.ground.util.protocol.http.HttpResponse;
 
@@ -12,4 +13,8 @@ public interface PurchaseManageService {
     HttpResponse purchaseProductList(PurchaseFormRequest purchaseFormRequest);
 
     HttpResponse purchaseForm(List<String> applyIds);
+
+    HttpResponse deletePurchaseFile(PurchaseFile purchaseFile);
+
+    HttpResponse purchaseFileList(String purchaseId);
 }
