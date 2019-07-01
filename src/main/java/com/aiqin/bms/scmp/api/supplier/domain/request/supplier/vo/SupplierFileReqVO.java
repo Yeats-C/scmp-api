21f5e3077c31ec1a737f9d76b5377c7bd3b0a7df
谢庆 <v_xieqing@aiqin.com>
2019-07-01 15:58:29 +0800

    开启定时任务

--- src/main/java/com/aiqin/bms/scmp/api/supplier/domain/request/supplier/vo/SupplierFileReqVO.java
+++ src/main/java/com/aiqin/bms/scmp/api/supplier/domain/request/supplier/vo/SupplierFileReqVO.java
@@ -1,5 +1,6 @@
 package com.aiqin.bms.scmp.api.supplier.domain.request.supplier.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -37,6 +38,7 @@ public class SupplierFileReqVO {
     private String fileName;
 
     @ApiModelProperty("创建时间")
+    @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
     private Date createTime;
 
     @ApiModelProperty("创建人")
