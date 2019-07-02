1439edd99ce9ab19abea9733246cb56891b09ebe
谢庆 <v_xieqing@aiqin.com>
2019-07-02 15:36:09 +0800

    审批回调配置

--- src/main/java/com/aiqin/bms/scmp/api/product/service/impl/AllocationServiceImpl.java
+++ src/main/java/com/aiqin/bms/scmp/api/product/service/impl/AllocationServiceImpl.java
@@ -32,7 +32,9 @@ import com.aiqin.bms.scmp.api.product.service.api.SupplierApiService;
 import com.aiqin.bms.scmp.api.supplier.dao.EncodingRuleDao;
 import com.aiqin.bms.scmp.api.supplier.domain.pojo.EncodingRule;
 import com.aiqin.bms.scmp.api.util.*;
+import com.aiqin.bms.scmp.api.workflow.annotation.WorkFlowAnnotation;
 import com.aiqin.bms.scmp.api.workflow.enumerate.WorkFlow;
+import com.aiqin.bms.scmp.api.workflow.helper.WorkFlowHelper;
 import com.aiqin.bms.scmp.api.workflow.vo.request.WorkFlowCallbackVO;
 import com.aiqin.bms.scmp.api.workflow.vo.request.WorkFlowVO;
 import com.aiqin.bms.scmp.api.workflow.vo.response.WorkFlowRespVO;
@@ -65,7 +67,8 @@ import java.util.stream.Collectors;
  */
 @Slf4j
 @Service
-public class AllocationServiceImpl extends BaseServiceImpl implements AllocationService  {
+@WorkFlowAnnotation(WorkFlow.APPLY_ALLOCATTION)
+public class AllocationServiceImpl extends BaseServiceImpl implements AllocationService, WorkFlowHelper {
 
 
     @Autowired
