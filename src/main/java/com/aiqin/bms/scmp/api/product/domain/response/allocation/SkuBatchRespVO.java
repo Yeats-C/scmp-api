4db1e7dd644930d531603a1f68bc46b68bfef6dc
谢庆 <v_xieqing@aiqin.com>
2019-07-06 13:49:40 +0800

    调拨导入增加批次库存

--- src/main/java/com/aiqin/bms/scmp/api/product/domain/response/allocation/SkuBatchRespVO.java
+++ src/main/java/com/aiqin/bms/scmp/api/product/domain/response/allocation/SkuBatchRespVO.java
@@ -1,12 +1,12 @@
 package com.aiqin.bms.scmp.api.product.domain.response.allocation;
 
-import com.aiqin.bms.scmp.api.product.domain.response.changeprice.BatchInfo;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.Date;
-import java.util.List;
 
 /**
  * Description:
@@ -16,40 +16,48 @@ import java.util.List;
  * @time: 20:47
  */
 @Data
+@ApiModel("调拨导入商品批次返回列表")
 public class SkuBatchRespVO {
 
     @ApiModelProperty("sku编码")
+    @JsonProperty("sku_code")
     private String skuCode;
 
     @ApiModelProperty("sku编码")
+    @JsonProperty("sku_name")
     private String skuName;
 
     @ApiModelProperty("仓库批次号编码")
+    @JsonProperty("batch_code")
     private String warehouseBatchNumber;
 
-    @ApiModelProperty("仓库批次号名称")
-    private String warehouseBatchName;
-
     @ApiModelProperty("仓库编码")
+    @JsonProperty("transport_center_code")
     private String transportCenterCode;
 
     @ApiModelProperty("仓库名称")
+    @JsonProperty("transport_center_name")
     private String transportCenterName;
 
     @ApiModelProperty("库房编码")
+    @JsonProperty("warehouse_code")
     private String warehouseCode;
 
     @ApiModelProperty("库房名称")
+    @JsonProperty("warehouse_name")
     private String warehouseName;
 
     @ApiModelProperty("批次备注")
+    @JsonProperty("batch_remark")
     private String batchRemark;
+
+    @ApiModelProperty("可用库存")
+    @JsonProperty("available_num")
+    private Long availableNum;
 
     @ApiModelProperty("生产日期")
     @JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd")
+    @JsonProperty("production_date")
     private Date productTime;
-
-    @ApiModelProperty("批次信息")
-    private List<BatchInfo> batchList;
 
 }
--- src/main/java/com/aiqin/bms/scmp/api/product/domain/response/changeprice/BatchInfo.java
+++ src/main/java/com/aiqin/bms/scmp/api/product/domain/response/changeprice/BatchInfo.java
@@ -1,6 +1,7 @@
 package com.aiqin.bms.scmp.api.product.domain.response.changeprice;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
@@ -16,28 +17,35 @@ import java.util.Date;
 @Data
 public class BatchInfo {
     @ApiModelProperty("仓库批次号编码")
+    @JsonProperty("batch_code")
     private String warehouseBatchNumber;
 
-    @ApiModelProperty("仓库批次号名称")
+    @ApiModelProperty(value = "仓库批次号名称",hidden = true)
     private String warehouseBatchName;
 
     @ApiModelProperty("仓库编码")
+    @JsonProperty("transport_center_code")
     private String transportCenterCode;
 
     @ApiModelProperty("仓库名称")
+    @JsonProperty("transport_center_name")
     private String transportCenterName;
 
     @ApiModelProperty("库房编码")
+    @JsonProperty("warehouse_code")
     private String warehouseCode;
 
     @ApiModelProperty("库房名称")
+    @JsonProperty("warehouse_name")
     private String warehouseName;
 
     @ApiModelProperty("批次备注")
+    @JsonProperty("batch_remark")
     private String batchRemark;
 
     @ApiModelProperty("生产日期")
     @JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd")
+    @JsonProperty("production_date")
     private Date productTime;
 
     public String getWarehouseBatchName() {
