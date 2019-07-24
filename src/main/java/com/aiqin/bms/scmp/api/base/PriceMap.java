820ddbacec3c9c646aca7ad7786425e8ab03eb8f
hechao <hechao>
2019-07-24 23:00:43 +0800

    商品导入todo

--- src/main/java/com/aiqin/bms/scmp/api/base/PriceMap.java
+++ src/main/java/com/aiqin/bms/scmp/api/base/PriceMap.java
@@ -1,7 +1,13 @@
 package com.aiqin.bms.scmp.api.base;
 
-public class PriceMap {
-    static {
+import com.aiqin.bms.scmp.api.product.domain.pojo.PriceChannel;
+import com.google.common.collect.Maps;
 
+import java.util.Map;
+
+public class PriceMap {
+    public static Map<String, PriceChannel> price = Maps.newHashMap();
+    static {
+//        price.put("爱亲渠道价",new SkuPriceDraftReqVO())
     }
 }
