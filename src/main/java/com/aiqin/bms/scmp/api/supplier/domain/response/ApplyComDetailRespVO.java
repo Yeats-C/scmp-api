1e144195685c3ba7b5f32169b1278a0a8da98e1d
hechao <hechao>
2019-08-01 18:04:18 +0800

    修改供应商导入bug

--- src/main/java/com/aiqin/bms/scmp/api/supplier/domain/response/ApplyComDetailRespVO.java
+++ src/main/java/com/aiqin/bms/scmp/api/supplier/domain/response/ApplyComDetailRespVO.java
@@ -1,6 +1,13 @@
 package com.aiqin.bms.scmp.api.supplier.domain.response;
 
 import com.aiqin.bms.scmp.api.supplier.domain.response.supplier.SupplyComDetailRespVO;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class ApplyComDetailRespVO extends SupplyComDetailRespVO {
+    @ApiModelProperty("申请修改时需要展示的供应商编码")
+    private String formalCode;
 }
