d10aa18e9f38cb9471a38ae5bd4192dda291423b
hechao <hechao>
2019-08-05 14:59:25 +0800

    修改文件bug,修改供应商评分bug

--- src/main/java/com/aiqin/bms/scmp/api/supplier/domain/pojo/ApplySupplierFile.java
+++ src/main/java/com/aiqin/bms/scmp/api/supplier/domain/pojo/ApplySupplierFile.java
@@ -1,6 +1,7 @@
 package com.aiqin.bms.scmp.api.supplier.domain.pojo;
 
 import com.aiqin.bms.scmp.api.common.CommonBean;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -32,12 +33,14 @@ public class ApplySupplierFile extends CommonBean {
     private String fileCode;
 
     @ApiModelProperty("创建时间")
+    @JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
     private Date createTime;
 
     @ApiModelProperty("创建人")
     private String createBy;
 
     @ApiModelProperty("修改时间")
+    @JsonFormat(timezone = "GMT+8",pattern = "yyyy-MM-dd HH:mm:ss")
     private Date updateTime;
 
     @ApiModelProperty("修改人")
