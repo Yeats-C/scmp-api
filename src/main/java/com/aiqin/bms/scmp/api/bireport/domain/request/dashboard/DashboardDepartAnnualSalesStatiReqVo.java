7a922eaca184e8aa6b90c0f44302b3fcb4f62d36
ch <2244692358@qq.com>
2019-08-07 14:59:53 +0800

    仪表盘字段修改

--- src/main/java/com/aiqin/bms/scmp/api/bireport/domain/request/dashboard/DashboardDepartAnnualSalesStatiReqVo.java
+++ src/main/java/com/aiqin/bms/scmp/api/bireport/domain/request/dashboard/DashboardDepartAnnualSalesStatiReqVo.java
@@ -1,9 +1,13 @@
 package com.aiqin.bms.scmp.api.bireport.domain.request.dashboard;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @ApiModel("年销售情况(部门)reqVo")
 @Data
 public class DashboardDepartAnnualSalesStatiReqVo {
+
+
 }
