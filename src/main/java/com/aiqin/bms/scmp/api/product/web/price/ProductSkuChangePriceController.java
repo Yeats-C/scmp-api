d078b8232b4acf9ad5ea33fd905a4161487b003d
hechao <hechao>
2019-08-13 19:08:21 +0800

    修改变价

--- src/main/java/com/aiqin/bms/scmp/api/product/web/price/ProductSkuChangePriceController.java
+++ src/main/java/com/aiqin/bms/scmp/api/product/web/price/ProductSkuChangePriceController.java
@@ -3,10 +3,7 @@ package com.aiqin.bms.scmp.api.product.web.price;
 import com.aiqin.bms.scmp.api.base.BasePage;
 import com.aiqin.bms.scmp.api.base.ResultCode;
 import com.aiqin.bms.scmp.api.common.BizException;
-import com.aiqin.bms.scmp.api.product.domain.request.changeprice.ProductSkuChangePriceImportReqVo;
-import com.aiqin.bms.scmp.api.product.domain.request.changeprice.ProductSkuChangePriceReqVO;
-import com.aiqin.bms.scmp.api.product.domain.request.changeprice.QueryProductSkuChangePriceReqVO;
-import com.aiqin.bms.scmp.api.product.domain.request.changeprice.QuerySkuInfoReqVO;
+import com.aiqin.bms.scmp.api.product.domain.request.changeprice.*;
 import com.aiqin.bms.scmp.api.product.domain.response.changeprice.*;
 import com.aiqin.bms.scmp.api.product.service.ProductSkuChangePriceService;
 import com.aiqin.bms.scmp.api.util.IdSequenceUtils;
@@ -212,6 +209,20 @@ public class ProductSkuChangePriceController {
             return HttpResponse.failure(ResultCode.SYSTEM_ERROR);
         }
     }
+//
+//    @ApiOperation("变价测算")
+//    @PostMapping("/priceMeasurement")
+//    public HttpResponse<List<PriceJog>> priceMeasurement(@RequestBody List<PriceMeasurementReqVO> req){
+//        try {
+//            return HttpResponse.success(productSkuChangePriceService.priceMeasurement(req));
+//        } catch (BizException e) {
+//            return HttpResponse.failure(e.getMessageId());
+//        }catch (Exception e){
+//            e.printStackTrace();
+//            return HttpResponse.failure(ResultCode.SYSTEM_ERROR);
+//        }
+//    }
+
     @ApiOperation("测试生成id")
     @PostMapping("/getid")
     public Long getid(){
