a4f3b9a3da3747d6002d77233b527a0dda200086
hechao <hechao>
2019-08-14 11:12:17 +0800

    增加字段。

--- src/main/java/com/aiqin/bms/scmp/api/product/domain/request/changeprice/ProductSkuChangePriceReqVO.java
+++ src/main/java/com/aiqin/bms/scmp/api/product/domain/request/changeprice/ProductSkuChangePriceReqVO.java
@@ -93,6 +93,15 @@ public class ProductSkuChangePriceReqVO {
     @ApiModelProperty("是否含有区域0否1是")
     private Integer extField5;
 
+    @ApiModelProperty("毛利率增加数")
+    private Long increaseCount;
+    @ApiModelProperty("毛利率减少数")
+    private Long decreaseCount;
+    @ApiModelProperty("毛利率增加额度")
+    private Long increaseGrossProfit;
+    @ApiModelProperty("毛利率减少额度")
+    private Long decreaseGrossProfit;
+
     @ApiModelProperty("sku列表信息")
     @NotEmpty(message = "sku信息不能为空")
     private List<ProductSkuChangePriceInfoReqVO> infoLists;
