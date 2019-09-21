b0371dfa790207f8ea82bd5d7c1aaa1b9d8fe266
wangximin@aiqin.com <wangximin@aiqin.com>
2019-09-21 14:29:31 +0800

    信息打印

--- src/main/java/com/aiqin/bms/scmp/api/purchase/service/impl/OrderCallbackServiceImpl.java
+++ src/main/java/com/aiqin/bms/scmp/api/purchase/service/impl/OrderCallbackServiceImpl.java
@@ -819,8 +819,8 @@ public class OrderCallbackServiceImpl implements OrderCallbackService {
             stockChangeRequest.setStockVoRequests(list);
             HttpResponse httpResponse = stockService.changeStock(stockChangeRequest);
             if (!MsgStatus.SUCCESS.equals(httpResponse.getCode())) {
-                LOGGER.error("dl回调:报损报溢减库存异常");
-                throw new GroundRuntimeException("dl回调:报损报溢减库存异常");
+                LOGGER.error("dl回调:调拨减库存异常");
+                throw new GroundRuntimeException("dl回调:减库存异常");
             }
             //生成入库单
             InboundReqSave inboundReqSave = handleTransferInbound(allocation, productSkuMap, inboundTypeEnum);
@@ -834,8 +834,8 @@ public class OrderCallbackServiceImpl implements OrderCallbackService {
             stockRequest.setStockVoRequests(inboundList);
             HttpResponse stockResponse = stockService.changeStock(stockRequest);
             if (!MsgStatus.SUCCESS.equals(stockResponse.getCode())) {
-                LOGGER.error("dl回调:报损报溢加库存异常");
-                throw new GroundRuntimeException("dl回调:报损报溢加库存异常");
+                LOGGER.error("dl回调:调拨加库存异常");
+                throw new GroundRuntimeException("dl回调:加库存异常");
             }
             allocation.setOutboundOderCode(outboundOderCode);
             allocation.setInboundOderCode(inboundOderCode);
