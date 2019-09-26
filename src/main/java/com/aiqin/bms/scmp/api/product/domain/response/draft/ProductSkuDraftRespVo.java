e695212aa74c00527914174e64a7e3d8a3d908a3
hechao <hechao>
2019-09-26 18:11:14 +0800

    修改shu待申请列表

--- src/main/java/com/aiqin/bms/scmp/api/product/domain/response/draft/ProductSkuDraftRespVo.java
+++ src/main/java/com/aiqin/bms/scmp/api/product/domain/response/draft/ProductSkuDraftRespVo.java
@@ -70,4 +70,71 @@ public class ProductSkuDraftRespVo {
     @ApiModelProperty(value = "来源 0供应链1供应商平台",hidden = true)
     private Integer originalCode;
 
+    @ApiModelProperty("类型")
+    private String goodsGifts;
+    @ApiModelProperty("SKU编号")
+    private String skuCode;
+    @ApiModelProperty("SKU名称")
+    private String skuName;
+    @ApiModelProperty("商品属性")
+    private String productPropertyName;
+    @ApiModelProperty("覆盖渠道")
+    private String coverage;
+    @ApiModelProperty("结算方式")
+    private String settlementMethodName;
+    @ApiModelProperty("厂商指导价")
+    private Integer manufacturerGuidePrice;
+    @ApiModelProperty("含税采购价")
+    private Integer taxIncludedPrice;
+    @ApiModelProperty("爱亲渠道价")
+    private Integer aiqinDistributionPrice;
+    @ApiModelProperty("萌贝树渠道价")
+    private Integer mengbeishuDistributionPrice;
+    @ApiModelProperty("小红马渠道价")
+    private Integer xiaohongmaDistributionPrice;
+    @ApiModelProperty("爱亲分销价")
+    private Integer aiqinChannelPrice;
+    @ApiModelProperty("萌贝树分销价")
+    private Integer mengbeishuChannelPrice;
+    @ApiModelProperty("小红马分销价")
+    private Integer xiaohongmaChannelPrice;
+    @ApiModelProperty("售价")
+    private Integer salePrice;
+    @ApiModelProperty("会员价")
+    private Integer memberPrice;
+    @ApiModelProperty("进项税率")
+    private Integer outputTaxRate;
+    @ApiModelProperty("销项税率")
+    private Integer inputTaxRate;
+    @ApiModelProperty("颜色")
+    private String colorName;
+    @ApiModelProperty("型号")
+    private String modelNumber;
+    @ApiModelProperty("保质天数")
+    private Integer qualityDate;
+    @ApiModelProperty("保质天数单位")
+    private String qualityNumber;
+    @ApiModelProperty("适用起始月龄")
+    private String applicableMonthAge;
+    @ApiModelProperty("唯一码管理")
+    private String uniqueCode;
+    @ApiModelProperty("供应商")
+    private String supplyUnitName;
+    @ApiModelProperty("库存规格")
+    private String spec;
+    @ApiModelProperty("库存单位")
+    private String unitName;
+    @ApiModelProperty("采购单位含量")
+    private Integer inBaseProductContent;
+    @ApiModelProperty("分销交易倍数")
+    private Integer zeroRemovalCoefficient;
+    @ApiModelProperty("销售条码")
+    private String salesCode;
+    @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+    @ApiModelProperty("修改时间")
+    private Date updateTime;
+
+    @ApiModelProperty("修改人")
+    private String updateBy;
+
 }
