b99bbcd02dd038f6ab05847eecf001df4a3d43e5
zhaoshuai <zhaoshuai@aiqin.com>
2019-10-16 17:15:01 +0800

    采购信息导出pdf

--- src/main/java/com/aiqin/bms/scmp/api/purchase/web/PurchaseApplyController.java
+++ src/main/java/com/aiqin/bms/scmp/api/purchase/web/PurchaseApplyController.java
@@ -139,4 +139,11 @@ public class PurchaseApplyController {
     public HttpResponse<PurchaseNewContrastResponse> purchaseContrast(@RequestBody PurchaseNewContrastRequest contrastRequest) {
         return purchaseApplyService.purchaseContrast(contrastRequest);
     }
+
+    @GetMapping("/pdf")
+    @ApiOperation("订货/收货单导出PDF模板")
+    public HttpResponse importPdf(@RequestParam("file_path") String filePath,
+                                  @RequestParam("purchase_order_code") String purchaseOrderCode) {
+        return purchaseApplyService.importPdf(filePath, purchaseOrderCode);
+    }
 }
