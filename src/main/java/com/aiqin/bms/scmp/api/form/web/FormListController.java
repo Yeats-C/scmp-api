472b01458e5276c22263f5b2de85e7dc5e4a99de
huangjiaojiao <huangjiaojiao@aiqin.com>
2019-11-25 19:25:08 +0800

    替换Integer

--- src/main/java/com/aiqin/bms/scmp/api/form/web/FormListController.java
+++ src/main/java/com/aiqin/bms/scmp/api/form/web/FormListController.java
@@ -17,46 +17,46 @@ public class FormListController {
     @Resource
     private FormListService formListService;
     
-    private final String receiptType = "scmp-system";
+//    private final String receiptType = "scmp-system";
 
     @PostMapping("/send")
     @ApiOperation("查询我的已发")
     HttpResponse findActBaseList(@RequestBody FormListRequest formListRequest) {
-        formListRequest.setReceiptType(receiptType);
+        formListRequest.setReceiptType(2);
         return formListService.findActBaseList(formListRequest);
     }
 
     @PostMapping("/wait")
     @ApiOperation("查询我的待办")
     HttpResponse findMyTaskList(@RequestBody FormListRequest formListRequest) {
-        formListRequest.setReceiptType(receiptType);
+        formListRequest.setReceiptType(2);
         return formListService.findMyTaskList(formListRequest);
     }
 
     @PostMapping("/done")
     @ApiOperation("查询我的已办")
     HttpResponse findMyDoTaskList(@RequestBody FormListRequest formListRequest) {
-        formListRequest.setReceiptType(receiptType);
+        formListRequest.setReceiptType(2);
         return formListService.findMyDoTaskList(formListRequest);
     }
 
     @PostMapping("/relate")
     @ApiOperation("查询抄送我的")
     HttpResponse findMyTaskListTaskDefinitionKeyWithzhsp(@RequestBody FormListRequest formListRequest) {
-        formListRequest.setReceiptType(receiptType);
+        formListRequest.setReceiptType(2);
         return formListService.findMyTaskListTaskDefinitionKeyWithzhsp(formListRequest);
     }
 
     @GetMapping("/count")
     @ApiOperation("查询数量")
     HttpResponse findTaskListCount(@RequestParam("person_id") String personId, @RequestParam(value = "process_key", required = false) String processKey) {
-        return formListService.findTaskListCount(personId,receiptType, processKey);
+        return formListService.findTaskListCount(personId,2, processKey);
     }
 
     @GetMapping("/process")
     @ApiOperation("审批类型列表")
     HttpResponse getProcessListByType() {
-        return formListService.getProcessListByType(receiptType);
+        return formListService.getProcessListByType(2);
     }
 
 }
