effaf41cb54ce1770b4a8b7a92fb3a799c4c2127
Maiiipzh <maiii1993@163.com>
2019-12-09 14:55:54 +0800

    Merge branch 'dev-wl' of http://git.aiqin.com/bms/scmp/scmp-api into master_mamingze
    # Conflicts:
    #	src/main/java/com/aiqin/bms/scmp/api/product/domain/pojo/ProductSkuCheckoutDraft.java
    #	src/main/java/com/aiqin/bms/scmp/api/product/service/impl/SkuInfoServiceImpl.java
    #	src/main/resources/com/aiqin/bms/scmp/api/product/mapper/ProductSkuDraftMapper.xml

--- src/main/java/com/aiqin/bms/scmp/api/product/domain/dto/changeprice/SaleCountDTO.java
+++ src/main/java/com/aiqin/bms/scmp/api/product/domain/dto/changeprice/SaleCountDTO.java
@@ -2,8 +2,10 @@ package com.aiqin.bms.scmp.api.product.domain.dto.changeprice;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 public class SaleCountDTO {
     private String skuCode;
-    private Integer saleNum = 0;
+    private BigDecimal saleNum = BigDecimal.ZERO;
 }
