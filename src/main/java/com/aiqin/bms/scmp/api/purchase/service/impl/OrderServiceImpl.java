2b0d9cd76249bcaba29dd2e9db85bde7150641e7
ch <2244692358@qq.com>
2020-05-19 20:26:31 +0800

    update

--- src/main/java/com/aiqin/bms/scmp/api/purchase/service/impl/OrderServiceImpl.java
+++ src/main/java/com/aiqin/bms/scmp/api/purchase/service/impl/OrderServiceImpl.java
@@ -486,12 +486,12 @@ public class OrderServiceImpl extends BaseServiceImpl implements OrderService {
 
                // 配送的情况下 调用wms
                 SaleSourcInfoSource saleSourcInfoSource = insertWms(vo,insertOutbound);
-            //    String url = urlConfig.WMS_API_URL+"/sale/source/outbound";
-            //    HttpClient httpClient = HttpClient.post(url).json(saleSourcInfoSource).timeout(200000);
-            //    HttpResponse orderDto = httpClient.action().result(HttpResponse.class);
-            //    if (!orderDto.getCode().equals(MessageId.SUCCESS_CODE)) {
-            //        return HttpResponse.failure(null, "调用wms失败,原因：" + orderDto.getMessage());
-            //    }
+                String url = urlConfig.WMS_API_URL+"/sale/source/outbound";
+                HttpClient httpClient = HttpClient.post(url).json(saleSourcInfoSource).timeout(200000);
+                HttpResponse orderDto = httpClient.action().result(HttpResponse.class);
+                if (!orderDto.getCode().equals(MessageId.SUCCESS_CODE)) {
+                    return HttpResponse.failure(null, "调用wms失败,原因：" + orderDto.getMessage());
+                }
             }
             logs.add(log);
         }
