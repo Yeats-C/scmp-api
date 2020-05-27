98d73e0aca6c267ed503e47fcf1fbe4130a35b4c
ch <2244692358@qq.com>
2020-05-27 16:42:25 +0800

    wms销售推送

--- src/main/java/com/aiqin/bms/scmp/api/purchase/domain/request/order/BatchWmsInfo.java
+++ src/main/java/com/aiqin/bms/scmp/api/purchase/domain/request/order/BatchWmsInfo.java
@@ -14,39 +14,39 @@ import java.util.Date;
 public class BatchWmsInfo {
 
     @ApiModelProperty(value = "批次号")
-    @JsonProperty("batch_code")
+    //@JsonProperty("batch_code")
     private String batchCode;
 
     @ApiModelProperty(value = "sku编码")
-    @JsonProperty("sku_code")
+    //@JsonProperty("sku_code")
     private String skuCode;
 
     @ApiModelProperty(value = "sku名称")
-    @JsonProperty("sku_name")
+    //@JsonProperty("sku_name")
     private String skuName;
 
     @ApiModelProperty(value = "生产日期")
-    @JsonProperty("prodcut_date")
+    //@JsonProperty("prodcut_date")
     private String prodcutDate;
 
     @ApiModelProperty(value = "批次备注")
-    @JsonProperty("batch_remark")
+    //@JsonProperty("batch_remark")
     private String batchRemark;
 
     @ApiModelProperty(value = "最小单位数量")
-    @JsonProperty("total_count")
+    //@JsonProperty("total_count")
     private Long totalCount;
 
     @ApiModelProperty(value = "实际最小单位数量")
-    @JsonProperty("actual_total_count")
+    //@JsonProperty("actual_total_count")
     private Long actualTotalCount;
 
     @ApiModelProperty(value = "行号")
-    @JsonProperty("line_code")
+    //@JsonProperty("line_code")
     private Integer lineCode;
 
     @ApiModelProperty(value = "过期日期")
-    @JsonProperty("be_overdue_data")
+    //@JsonProperty("be_overdue_data")
     private String beOverdueData;
 
 }
