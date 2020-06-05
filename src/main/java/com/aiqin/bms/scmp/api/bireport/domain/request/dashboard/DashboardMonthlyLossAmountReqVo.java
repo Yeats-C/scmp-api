ea7ec1b8c35395ef00b427520993a759ed99e8c9
zhaoshuai <zhaoshuai@aiqin.com>
2020-06-05 10:10:31 +0800

    删除无用的请求类

--- src/main/java/com/aiqin/bms/scmp/api/bireport/domain/request/dashboard/DashboardMonthlyLossAmountReqVo.java
+++
@@ -1,9 +0,0 @@
-package com.aiqin.bms.scmp.api.bireport.domain.request.dashboard;
-
-import io.swagger.annotations.ApiModel;
-import lombok.Data;
-
-@ApiModel("月亏损")
-@Data
-public class DashboardMonthlyLossAmountReqVo {
-}
--- src/main/java/com/aiqin/bms/scmp/api/bireport/domain/request/dashboard/DashboardMonthlySalesStatiAccReqVo.java
+++
@@ -1,9 +0,0 @@
-package com.aiqin.bms.scmp.api.bireport.domain.request.dashboard;
-
-import io.swagger.annotations.ApiModel;
-import lombok.Data;
-
-@ApiModel("月销售情况（月累计）reqVo")
-@Data
-public class DashboardMonthlySalesStatiAccReqVo {
-}
