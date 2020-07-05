03501cf6ea88d568ce6d2bcfa51b28cb44824c67
zhaoshuai <zhaoshuai@aiqin.com>
2020-07-05 17:53:36 +0800

    退货对接dl

--- src/main/java/com/aiqin/bms/scmp/api/purchase/domain/request/dl/EchoOrderRequest.java
+++ src/main/java/com/aiqin/bms/scmp/api/purchase/domain/request/dl/EchoOrderRequest.java
@@ -12,13 +12,13 @@ import java.util.List;
 @ApiModel("DL- 回传单据主参数")
 public class EchoOrderRequest {
 
-    @ApiModelProperty(value="单据单号")
-    @JsonProperty("order_code")
-    private String orderCode;
-
     @ApiModelProperty(value="单据单id")
     @JsonProperty("order_id")
     private String orderId;
+
+    @ApiModelProperty(value="单据单号")
+    @JsonProperty("order_code")
+    private String orderCode;
 
     @ApiModelProperty(value="操作时间")
     @JsonProperty("operation_time")
@@ -35,6 +35,10 @@ public class EchoOrderRequest {
     @ApiModelProperty(value="操作人名称")
     @JsonProperty("operation_name")
     private String operationName;
+
+    @ApiModelProperty(value="业务形式 1门店、2批发、3线上业务、4线下业务、5优选业务、6天猫业务")
+    @JsonProperty("business_form")
+    private Integer businessForm;
 
     @ApiModelProperty(value="商品信息")
     @JsonProperty("product_list")
