a01af586bb662871c98837ec3074bdbc2fb413af
niuhuan <admin@example.com>
2020-08-05 13:35:53 +0800

    所有获取编码地方全部替换成从redis中获取

--- src/test/java/com/aiqin/bms/scmp/api/service/test/SynchronizationStockServiceImplTest.java
+++ src/test/java/com/aiqin/bms/scmp/api/service/test/SynchronizationStockServiceImplTest.java
@@ -3,6 +3,7 @@ package com.aiqin.bms.scmp.api.service.test;
 import com.aiqin.bms.scmp.api.ScmpApiBootApplication;
 import com.aiqin.bms.scmp.api.base.PagesRequest;
 import com.aiqin.bms.scmp.api.purchase.jobs.SynchronizationStockService;
+import com.aiqin.bms.scmp.api.util.CodeUtils;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
@@ -20,6 +21,9 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * <pre>
@@ -56,11 +60,47 @@ public class SynchronizationStockServiceImplTest {
     @Resource
     private SynchronizationStockService synchronizationStockService;
 
+    @Resource
+    private CodeUtils codeUtils;
+
     @Test
     public void synchronizationStock() {
         PagesRequest request = new PagesRequest();
         request.setPageNo(1);
         request.setPageSize(500);
         synchronizationStockService.synchronizationStock(request);
+    }
+
+    @Test
+    public  void test2 (){
+        ExecutorService service = Executors.newFixedThreadPool(5);
+        final CountDownLatch latch = new CountDownLatch(5);
+        for (int i = 0; i < 5; i++) {
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        System.out.println("子线程" + Thread.currentThread().getName() + "开始执行");
+                        Thread.sleep((long) (Math.random() * 10000));
+                        System.out.println("子线程"+Thread.currentThread().getName()+"执行完成");
+                        latch.countDown();//当前线程调用此方法，则计数减一
+
+                        String redisCode = codeUtils.getRedisCode("PROMOTION_NO");
+                        System.out.println("==========="+redisCode);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+            service.execute(runnable);
+        }
+
+        try {
+            System.out.println("主线程"+Thread.currentThread().getName()+"等待子线程执行完成...");
+            latch.await();//阻塞当前线程，直到计数器的值为0
+            System.out.println("主线程"+Thread.currentThread().getName()+"开始执行...");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
