2cba618ec2330c58ff59f7b005a5424765ea4517
zhaoshuai <zhaoshuai@aiqin.com>
2020-08-05 11:34:45 +0800

    更改推送dl 日期库存数据问题

--- src/main/java/com/aiqin/bms/scmp/api/abutment/jobs/impl/DlMonthStockServiceImpl.java
+++ src/main/java/com/aiqin/bms/scmp/api/abutment/jobs/impl/DlMonthStockServiceImpl.java
@@ -1,18 +1,11 @@
 package com.aiqin.bms.scmp.api.abutment.jobs.impl;
 
 import com.aiqin.bms.scmp.api.abutment.domain.request.dl.MonthStockRequest;
-import com.aiqin.bms.scmp.api.abutment.domain.response.DLResponse;
 import com.aiqin.bms.scmp.api.abutment.jobs.DlMonthStockService;
 import com.aiqin.bms.scmp.api.abutment.service.ParameterAssemblyService;
 import com.aiqin.bms.scmp.api.product.dao.StockDayBatchDao;
-import com.aiqin.bms.scmp.api.product.domain.pojo.StockDayBatch;
-import com.aiqin.bms.scmp.api.supplier.dao.warehouse.WarehouseDao;
-import com.aiqin.bms.scmp.api.supplier.domain.request.warehouse.dto.WarehouseDTO;
 import com.aiqin.bms.scmp.api.util.CollectionUtils;
-import com.aiqin.bms.scmp.api.util.DLHttpClientUtil;
-import com.aiqin.ground.util.json.JsonUtil;
 import com.google.common.collect.Lists;
-import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,9 +14,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Service
 public class DlMonthStockServiceImpl implements DlMonthStockService {
@@ -35,71 +26,30 @@ public class DlMonthStockServiceImpl implements DlMonthStockService {
 
     @Resource
     private StockDayBatchDao stockDayBatchDao;
-    @Resource
-    private WarehouseDao warehouseDao;
     @Value("${dl.url}")
     private String DL_URL;
-    @Resource
-    private DLHttpClientUtil dlHttpClientUtil;
     @Resource
     @Lazy
     private ParameterAssemblyService parameterAssemblyService;
 
     @Override
-    @Scheduled(cron = "0 0/20 * * * ?")
+    //@Scheduled(cron = "0 0/20 * * * ?")
     public void monthStockDl(){
 
-        Map<String, WarehouseDTO> warehouseMap = new HashMap<>();
-        WarehouseDTO warehouse;
-        List<WarehouseDTO> warehouses = warehouseDao.warehouseList();
-        for(WarehouseDTO ware :  warehouses){
-            if(warehouseMap.get(ware.getWarehouseCode()) == null){
-                warehouseMap.put(ware.getWarehouseCode(), warehouseDao.getWarehouseByCode(ware.getWarehouseCode()));
-            }
-        }
-        MonthStockRequest monthStockRequest;
         List<MonthStockRequest> list = Lists.newArrayList();
 
         // 查询德邦所有的库存信息
-        List<StockDayBatch> stockDayBatches = stockDayBatchDao.stockDayByDl(DEBANG);
+        List<MonthStockRequest> stockDayBatches = stockDayBatchDao.stockDayByDl(DEBANG);
         if(CollectionUtils.isNotEmptyCollection(stockDayBatches)){
-            for(StockDayBatch stockMonth : stockDayBatches){
-                monthStockRequest = new MonthStockRequest();
-                monthStockRequest.setSkuCode(stockMonth.getSkuCode());
-                monthStockRequest.setBatchCode(stockMonth.getBatchCode());
-                monthStockRequest.setBatchCount(stockMonth.getBatchCount());
-                monthStockRequest.setWmsType(DEBANG);
-                if(StringUtils.isNotBlank(stockMonth.getWarehouseCode())){
-                    warehouse = warehouseMap.get(stockMonth.getWarehouseCode());
-                    monthStockRequest.setWarehouseCode(warehouse.getWmsWarehouseId());
-                    monthStockRequest.setWarehouseName(warehouse.getWmsWarehouseName());
-                    monthStockRequest.setWmsWarehouseType(warehouse.getWmsWarehouseType());
-                }else {
-                    LOGGER.info("同步dl日期批次未查询到德邦对应的库房：{}", stockMonth.getWarehouseCode());
-                }
-                list.add(monthStockRequest);
-            }
+            list.addAll(stockDayBatches);
+            LOGGER.info("同步DL日期库存数据，德邦数量：{}", stockDayBatches.size());
         }
 
         // 查询京东所有的库存信息
-        List<StockDayBatch> stockDayBatchJds = stockDayBatchDao.stockDayByDl(JINGDONG);
+        List<MonthStockRequest> stockDayBatchJds = stockDayBatchDao.stockDayByDl(JINGDONG);
         if(CollectionUtils.isNotEmptyCollection(stockDayBatchJds)){
-            for(StockDayBatch stockMonth : stockDayBatchJds){
-                monthStockRequest = new MonthStockRequest();
-                monthStockRequest.setSkuCode(stockMonth.getSkuCode());
-                monthStockRequest.setBatchCode(stockMonth.getBatchCode());
-                monthStockRequest.setBatchCount(stockMonth.getBatchCount());
-                monthStockRequest.setWmsType(JINGDONG);
-                if(StringUtils.isNotBlank(stockMonth.getWarehouseCode())){
-                    warehouse = warehouseMap.get(stockMonth.getWarehouseCode());
-                    monthStockRequest.setWarehouseCode(warehouse.getWmsWarehouseId());
-                    monthStockRequest.setWarehouseName(warehouse.getWmsWarehouseName());
-                    monthStockRequest.setWmsWarehouseType(warehouse.getWmsWarehouseType());
-                }else {
-                    LOGGER.info("同步dl月份批次未查询京东到对应的库房：{}", stockMonth.getWarehouseCode());
-                }
-                list.add(monthStockRequest);
-            }
+            list.addAll(stockDayBatchJds);
+            LOGGER.info("同步DL日期库存数据，京东数量：{}", stockDayBatchJds.size());
         }
         LOGGER.info("推送DL月份批次数据条数：{}", list.size());
         parameterAssemblyService.monthStockDlParameter(list);
